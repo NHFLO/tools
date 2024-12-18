b1fe7aa186a27af01c32f2318b75f28e6dcf54fe
Bas des Tombe <bdestombe@gmail.com>
2024-12-18 10:10:14 +0100

    Flexibility towards getting your grid from ds_regis, ix, modelgrid

--- src/nhflotools/pwnlayers2/layers.py
+++ src/nhflotools/pwnlayers2/layers.py
@@ -1,20 +1,21 @@
 """Module containing functions to retrieve PWN bodemlagen."""
 
 import logging
-from contextlib import redirect_stdout
 from pathlib import Path
 
 import geopandas as gpd
 import numpy as np
 import pykrige.ok
 import xarray as xr
+from flopy.discretization.vertexgrid import VertexGrid
 from flopy.utils.gridintersect import GridIntersect
+from nlmod.dims.grid import modelgrid_from_ds
 from shapely.ops import unary_union
 
 logger = logging.getLogger(__name__)
 
 
-def get_pwn_aquitard_data(ds_regis: xr.Dataset, data_dir: Path, ix: GridIntersect, transition_length: float) -> dict:
+def get_pwn_aquitard_data(ds_regis: xr.Dataset, ix: GridIntersect, modelgrid: VertexGrid, data_dir: Path, transition_length: float) -> dict:
     """
     Interpolate the thickness of the aquitard layers and the top of the aquitard layers using Kriging.
 
@@ -29,10 +30,12 @@ def get_pwn_aquitard_data(ds_regis: xr.Dataset, data_dir: Path, ix: GridIntersec
     ----------
     ds_regis : xr.Dataset
         The REGIS modellayer that contains the vertex grid.
-    data_dir : Path
-        The directory containing the data. Contains folders `dikte_aquitard` and `top_aquitard`.
     ix : flopy.utils.GridIntersect
         The index of the model grid.
+    modelgrid : flopy.discretization.VertexGrid
+        The model grid.
+    data_dir : Path
+        The directory containing the data. Contains folders `dikte_aquitard` and `top_aquitard`.
     transition_length : float
         The length of the transition zone in meters.
 
@@ -43,6 +46,13 @@ def get_pwn_aquitard_data(ds_regis: xr.Dataset, data_dir: Path, ix: GridIntersec
     """
     verbose = logger.level <= logging.DEBUG
 
+    if ix is None and modelgrid is None and ds_regis is not None:
+        modelgrid = modelgrid_from_ds(ds_regis)
+
+    if ix is None and modelgrid is not None:
+        ix = GridIntersect(modelgrid, method="vertex")
+
+    ncell = len(ix.mfgrid.cell2d)
     layer_names = ["S11", "S12", "S13", "S21", "S22", "S31", "S32"]
     data = {}
 
@@ -54,7 +64,7 @@ def get_pwn_aquitard_data(ds_regis: xr.Dataset, data_dir: Path, ix: GridIntersec
 
         multipolygon = unary_union(gdf_mask.geometry)
         ids = ix.intersect(multipolygon, contains_centroid=False, min_area_fraction=0.5).cellids.astype(int)
-        data[f"{name}_mask"] = np.zeros(ds_regis.sizes["icell2d"], dtype=bool)
+        data[f"{name}_mask"] = np.zeros(ncell, dtype=bool)
         data[f"{name}_mask"][ids] = True
 
         # Compute where the layer transitions to REGIS
@@ -62,43 +72,43 @@ def get_pwn_aquitard_data(ds_regis: xr.Dataset, data_dir: Path, ix: GridIntersec
         ids_trans = ix.intersect(
             multipolygon_transition, contains_centroid=False, min_area_fraction=0.5
         ).cellids.astype(int)
-        data[f"{name}_transition"] = np.zeros(ds_regis.sizes["icell2d"], dtype=bool)
+        data[f"{name}_transition"] = np.zeros(ncell, dtype=bool)
         data[f"{name}_transition"][ids_trans] = True
 
         # Interpolate thickness points using Kriging
         fp_pts = data_dir / "dikte_aquitard" / f"D{name}" / f"D{name}_interpolation_points.geojson"
         gdf_pts = gpd.read_file(fp_pts)
-
-        with redirect_stdout(logging.StreamHandler(logger)):
-            ok = pykrige.ok.OrdinaryKriging(
-                gdf_pts.geometry.x.values,
-                gdf_pts.geometry.y.values,
-                gdf_pts.value.values,
-                variogram_model="linear",
-                verbose=verbose,
-                enable_plotting=verbose,
-            )
-        xq = ds_regis.x.values[~data[f"{name}_mask"]]
-        yq = ds_regis.y.values[~data[f"{name}_mask"]]
+        ok = pykrige.ok.OrdinaryKriging(
+            gdf_pts.geometry.x.values,
+            gdf_pts.geometry.y.values,
+            gdf_pts.value.values,
+            variogram_model="linear",
+            verbose=verbose,
+            enable_plotting=False,
+        )
+        xq = ix.mfgrid.xcellcenters[~data[f"{name}_mask"]]
+        yq = ix.mfgrid.ycellcenters[~data[f"{name}_mask"]]
         kriging_result = ok.execute("points", xq, yq)
-        data[f"D{name}_value"] = np.where(~data[f"{name}_mask"], kriging_result[0], 0.)
-        data[f"D{name}_value_unc"] = np.where(~data[f"{name}_mask"], kriging_result[1], np.nan)
+        data[f"D{name}_value"] = np.zeros(ncell)
+        data[f"D{name}_value"][~data[f"{name}_mask"]] = kriging_result[0]
+        data[f"D{name}_value_unc"] = np.zeros(ncell)
+        data[f"D{name}_value_unc"][~data[f"{name}_mask"]] = kriging_result[1]
 
         # Interpolate top aquitard points using Kriging
         fp_pts = data_dir / "top_aquitard" / f"T{name}" / f"T{name}_interpolation_points.geojson"
         gdf_pts = gpd.read_file(fp_pts)
-
-        with redirect_stdout(logging.StreamHandler(logger)):
-            ok = pykrige.ok.OrdinaryKriging(
-                gdf_pts.geometry.x.values,
-                gdf_pts.geometry.y.values,
-                gdf_pts.value.values,
-                variogram_model="linear",
-                verbose=verbose,
-                enable_plotting=verbose,
-            )
+        ok = pykrige.ok.OrdinaryKriging(
+            gdf_pts.geometry.x.values,
+            gdf_pts.geometry.y.values,
+            gdf_pts.value.values,
+            variogram_model="linear",
+            verbose=verbose,
+            enable_plotting=False,
+        )
         kriging_result = ok.execute("points", xq, yq)
-        data[f"T{name}_value"] = np.where(~data[f"{name}_mask"], kriging_result[0], np.nan)
-        data[f"T{name}_value_unc"] = np.where(~data[f"{name}_mask"], kriging_result[1], np.nan)
+        data[f"T{name}_value"] = np.zeros(ncell)
+        data[f"T{name}_value"][~data[f"{name}_mask"]] = kriging_result[0]
+        data[f"T{name}_value_unc"] = np.zeros(ncell)
+        data[f"T{name}_value_unc"][~data[f"{name}_mask"]] = kriging_result[1]
 
     return data
