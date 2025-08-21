fix: correct logic in get_pwn_aquitard_data to compute presence of layers and update masking

src/nhflotools/pwnlayers2/layers.py
@@ -78,7 +78,7 @@ def get_pwn_aquitard_data(
     data = {}
 
     for name in layer_names:
-        # Compute where the layer is _not_ present
+        # Compute where the layer __is__ present
         logger.info("Interpolating aquitard layer %s data and its transition zone", name)
         fp_mask = data_dir / "dikte_aquitard" / f"D{name}" / f"D{name}_mask_combined.geojson"
         gdf_mask = gpd.read_file(fp_mask)
@@ -89,6 +89,12 @@ def get_pwn_aquitard_data(
             contains_centroid=False,
             min_area_fraction=0.5,
         )
+
+        f, ax = nlmod.plot.get_map(ds.extent, base=1e4)
+        ax.set_aspect("equal", adjustable="box")
+        pc = nlmod.plot.data_array(data[f"{name}_mask"], ds=ds)
+        nlmod.plot.modelgrid(ds, ax=ax, lw=0.25, alpha=0.5, color="k")
+        gdf_mask.plot(ax=ax, facecolor="red", alpha=0.5, edgecolor="r")
 
         # Compute where the layer transitions to REGIS
         multipolygon = unary_union(gdf_mask.geometry)
@@ -112,13 +118,13 @@ def get_pwn_aquitard_data(
             verbose=verbose,
             enable_plotting=False,
         )
-        xq = ix.mfgrid.xcellcenters[~data[f"{name}_mask"]]
-        yq = ix.mfgrid.ycellcenters[~data[f"{name}_mask"]]
+        xq = ix.mfgrid.xcellcenters[data[f"{name}_mask"]]
+        yq = ix.mfgrid.ycellcenters[data[f"{name}_mask"]]
         kriging_result = ok.execute("points", xq, yq)
         data[f"D{name}_value"] = nlmod.util.get_da_from_da_ds(ds, dims=("icell2d",), data=0.0)
-        data[f"D{name}_value"][~data[f"{name}_mask"]] = kriging_result[0]
+        data[f"D{name}_value"][data[f"{name}_mask"]] = kriging_result[0]
         data[f"D{name}_value_unc"] = nlmod.util.get_da_from_da_ds(ds, dims=("icell2d",), data=np.nan)
-        data[f"D{name}_value_unc"][~data[f"{name}_mask"]] = kriging_result[1]
+        data[f"D{name}_value_unc"][data[f"{name}_mask"]] = kriging_result[1]
 
         # Interpolate top aquitard points using Kriging
         fp_pts = data_dir / "top_aquitard" / f"T{name}" / f"T{name}_interpolation_points.geojson"
@@ -133,9 +139,9 @@ def get_pwn_aquitard_data(
         )
         kriging_result = ok.execute("points", xq, yq)
         data[f"T{name}_value"] = nlmod.util.get_da_from_da_ds(ds, dims=("icell2d",), data=np.nan)
-        data[f"T{name}_value"][~data[f"{name}_mask"]] = kriging_result[0]
+        data[f"T{name}_value"][data[f"{name}_mask"]] = kriging_result[0]
         data[f"T{name}_value_unc"] = nlmod.util.get_da_from_da_ds(ds, dims=("icell2d",), data=np.nan)
-        data[f"T{name}_value_unc"][~data[f"{name}_mask"]] = kriging_result[1]
+        data[f"T{name}_value_unc"][data[f"{name}_mask"]] = kriging_result[1]
 
     return data
 